137c276e59162dd445b622b6213c9cef6fc318c1
David North <bandanna2k@gmail.com>
2024-07-25 22:04:06 +1200

    Education - Flyway with a Test Container

--- java/testContainers/src/education/testContainers/Main.java
+++ java/testContainers/src/education/testContainers/Main.java
@@ -1,11 +1,19 @@
 package education.testContainers;
 
+import com.github.dockerjava.api.model.VolumesFrom;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.Configuration;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+
+import static java.util.Collections.*;
 
 public class Main
 {
@@ -23,6 +31,13 @@ public class Main
         )
         {
             genericContainer.start();
+
+            int mysqlPort = genericContainer.getMappedPort(3306);
+            String url = STR."jdbc:mysql://localhost:\{mysqlPort}/company?createDatabaseIfNotExist=true";
+            Flyway flyway = Flyway.configure()
+                    .dataSource(url, "root", "password")
+                    .load();
+            flyway.migrate();
 
             System.out.println("Press enter.");
             reader.readLine();
