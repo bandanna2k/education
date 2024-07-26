22d765e9500d444ab9ec93bb65df6dd2effc221b
David North <bandanna2k@gmail.com>
2024-07-26 20:48:03 +1200

    Education - Return accidentally deleted migration script

--- java/testContainers/resources/db/migration/V1.0__CreateBalanceTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE balances
-(
-    `account_id`        BIGINT          NOT NULL    PRIMARY KEY,
-    `balance`           DECIMAL(15,2)   NOT NULL
-)
-ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
