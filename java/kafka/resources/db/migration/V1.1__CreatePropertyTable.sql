3ed1b7147b1f4deb5b2cf2fe70cb03b42ebb01cc
David North <bandanna2k@gmail.com>
2024-07-29 21:57:33 +1200

    Education - Add properties table.

---
+++ java/kafka/resources/db/migration/V1.1__CreatePropertyTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE properties
+(
+    `key`             VARCHAR(100)      NOT NULL    PRIMARY KEY,
+    `value`           VARCHAR(500)      NOT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
