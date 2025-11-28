8893489abc3483483247c396291bb4c25dd7fad6
David North <bandanna2k@gmail.com>
2025-11-28 23:41:01 +1300

    Education - Local LLM - Working example.
    (cherry picked from commit 30d0b009017600edfcc585420e8521d8010b7c1c)

--- ai/local-llm/src/test/java/education/localai/LocalLLMBase.java
+++ ai/local-llm/src/test/java/education/localai/LocalLLMBase.java
@@ -4,6 +4,7 @@ import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.GenericContainer;
@@ -15,17 +16,19 @@ class LocalLLMBase {
 
     @BeforeAll
     static void beforeAll() {
-        llmContainer = new GenericContainer<>(DockerImageName.parse("mysql"))
+        llmContainer = new GenericContainer<>(DockerImageName.parse("ollama-with-llama3_2"))
                 .withCreateContainerCmdModifier(cmd -> cmd
                         .withName("ollama")
                         .withHostConfig(
                                 new HostConfig().withPortBindings(
                                         new PortBinding(Ports.Binding.bindPort(11434), new ExposedPort(11434))))
                 );
+        llmContainer.start();
     }
 
-    @AfterEach
-    void tearDown() {
+    @AfterAll
+    static void afterAll()
+    {
         llmContainer.stop();
     }
 }
