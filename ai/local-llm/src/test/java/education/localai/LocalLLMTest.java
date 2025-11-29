2c4aaadc1ecc4dade1d83bc9ff369aaabf6df8d3
David North <bandanna2k@gmail.com>
2025-11-29 18:33:24 +1300

    Education - Local LLM - Improve test.

--- ai/local-llm/src/test/java/education/localai/LocalLLMTest.java
+++ ai/local-llm/src/test/java/education/localai/LocalLLMTest.java
@@ -8,6 +8,8 @@ import io.github.ollama4j.Ollama;
 import io.github.ollama4j.exceptions.OllamaException;
 import io.github.ollama4j.models.generate.OllamaGenerateRequest;
 import io.github.ollama4j.models.response.OllamaResult;
+import io.github.ollama4j.utils.Options;
+import io.github.ollama4j.utils.OptionsBuilder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,6 +32,9 @@ Answer:""";
     private static final String QUESTION3 = """
 Question: Give me the most difficult question you can think off, maximum 5 words?
 Answer:""";
+    private static final String QUESTION4 = """
+Question: What should we have for dinner tonight?
+Answer:""";
 
 
     @ParameterizedTest
@@ -37,15 +42,25 @@ Answer:""";
             QUESTION1,
             "Context: My name is David. " + QUESTION1,
             QUESTION2,
-            QUESTION3
+            QUESTION3,
+            QUESTION4
     })
     void shouldGetHeightOfTheTennisNet(final String prompt) throws OllamaException
     {
+        int seed = 1;
+
         Ollama ollama = new Ollama("http://localhost:11434/");
+        ollama.setRequestTimeoutSeconds(10_000);
+
         ollama.listModels().forEach(model -> System.out.println(model.getName()));
+
+        Options options = new OptionsBuilder()
+                .setSeed(seed)
+                .build();
         OllamaGenerateRequest request = OllamaGenerateRequest.builder()
                 .withModel("llama3.2")
                 .withPrompt(prompt)
+                .withOptions(options)
                 .build();
         OllamaResult result = ollama.generate(request, null);
         System.out.println(result.getResponse());
