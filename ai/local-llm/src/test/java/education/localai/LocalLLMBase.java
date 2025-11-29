c4912d3f18a4547954544ec7a25cb9a3dd989158
David North <bandanna2k@gmail.com>
2025-11-30 10:44:29 +1300

    Education - Local LLM - Ask more questions

--- ai/local-llm/src/test/java/education/localai/LocalLLMBase.java
+++ ai/local-llm/src/test/java/education/localai/LocalLLMBase.java
@@ -4,6 +4,9 @@ import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
+import io.github.ollama4j.Ollama;
+import io.github.ollama4j.utils.Options;
+import io.github.ollama4j.utils.OptionsBuilder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,6 +16,8 @@ import org.testcontainers.utility.DockerImageName;
 class LocalLLMBase {
 
     private static GenericContainer<?> llmContainer;
+    protected static Ollama ollama;
+    protected static Options seededOptions;
 
     @BeforeAll
     static void beforeAll() {
@@ -24,6 +29,14 @@ class LocalLLMBase {
                                         new PortBinding(Ports.Binding.bindPort(11434), new ExposedPort(11434))))
                 );
         llmContainer.start();
+
+        ollama = new Ollama("http://localhost:11434/");
+        ollama.setRequestTimeoutSeconds(10_000);
+
+        int seed = 2;
+        seededOptions = new OptionsBuilder()
+                .setSeed(seed)
+                .build();
     }
 
     @AfterAll
--- ai/local-llm/src/test/java/education/localai/LocalLLMTest.java
+++ ai/local-llm/src/test/java/education/localai/LocalLLMTest.java
@@ -1,68 +1,62 @@
 package education.localai;
 
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
-import io.github.ollama4j.Ollama;
 import io.github.ollama4j.exceptions.OllamaException;
 import io.github.ollama4j.models.generate.OllamaGenerateRequest;
 import io.github.ollama4j.models.response.OllamaResult;
-import io.github.ollama4j.utils.Options;
-import io.github.ollama4j.utils.OptionsBuilder;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import java.io.IOException;
 
 public class LocalLLMTest extends LocalLLMBase {
 
-    private static final String QUESTION1 = """
-Question: What is my name?
-Answer:""";
-    private static final String QUESTION2 = """
-Question: What is the height of a tennis net in the middle?
-Answer:""";
-    private static final String QUESTION3 = """
-Question: Give me the most difficult question you can think off, maximum 5 words?
-Answer:""";
-    private static final String QUESTION4 = """
-Question: What should we have for dinner tonight?
-Answer:""";
-
-
     @ParameterizedTest
     @ValueSource(strings = {
-            QUESTION1,
-            "Context: My name is David. " + QUESTION1,
-            QUESTION2,
-            QUESTION3,
-            QUESTION4
+            "Question: What is my name? Answer:",
+            "Context: My name is David. Question: What is my name? Answer:",
+            "Question: What is the height of a tennis net in the middle? Answer:",
+            "Question: How much do you know? Answer:",
+            "Question: What should we have for dinner tonight? Answer:",
+            "Question: Can you write a paragraph about the Mary Celeste? Answer:",
     })
-    void shouldGetHeightOfTheTennisNet(final String prompt) throws OllamaException
+    void shouldAnswerQuestions(final String prompt) throws OllamaException
     {
-        int seed = 1;
-
-        Ollama ollama = new Ollama("http://localhost:11434/");
-        ollama.setRequestTimeoutSeconds(10_000);
-
-        ollama.listModels().forEach(model -> System.out.println(model.getName()));
-
-        Options options = new OptionsBuilder()
-                .setSeed(seed)
-                .build();
         OllamaGenerateRequest request = OllamaGenerateRequest.builder()
                 .withModel("llama3.2")
                 .withPrompt(prompt)
-                .withOptions(options)
+                .withOptions(seededOptions)
                 .build();
         OllamaResult result = ollama.generate(request, null);
         System.out.println(result.getResponse());
+    }
+
+    @Test
+    void answerOwnQuestion() throws OllamaException
+    {
+        String difficultQuestion = "Question: Give me the most difficult question you can think off, maximum 5 words? Answer:";
+        String generatedDifficultQuestion = askQuestion(difficultQuestion);
+        System.out.println(generatedDifficultQuestion);
+        System.out.println(askQuestion(generatedDifficultQuestion));
+    }
+    private String askQuestion(String question) throws OllamaException
+    {
+        OllamaGenerateRequest request = OllamaGenerateRequest.builder()
+                .withModel("llama3.2")
+                .withPrompt(question)
+                .withOptions(seededOptions)
+                .build();
+        OllamaResult result = ollama.generate(request, null);
+        return result.getResponse();
+    }
+
+    @Test
+    void shouldListModels() throws OllamaException
+    {
+        ollama.listModels().forEach(model -> System.out.println(model.getName()));
+    }
+
+    @Test
+    void shouldGenerateImage() throws OllamaException
+    {
+        System.out.println(askQuestion("Generate a random jpg in Base64 encoding of a dog, 640 pixels by 480 pixels? Please verify that the answer can be decoded."));
     }
 }
