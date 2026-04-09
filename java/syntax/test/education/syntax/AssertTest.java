a1585eba88ccb0061d6b0e111697fad474208f27
David North <bandanna2k@gmail.com>
2026-04-10 09:19:33 +1200

    Education - Syntax - Multiple asserts

--- java/syntax/test/education/syntax/AssertTest.java
+++ java/syntax/test/education/syntax/AssertTest.java
@@ -1,11 +1,20 @@
 package education.syntax;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class AssertTest
 {
+    @BeforeAll
+    static void beforeAll() {
+        System.setProperty("CREATE_NEW_OBJECT", "true");
+    }
+
     @Test
     public void shouldThrowOnAssertNotMet()
     {
@@ -14,5 +23,14 @@ public class AssertTest
                 })
                 .isInstanceOf(AssertionError.class)
                 .withFailMessage("Made up assertion not met.");
+    }
+
+    @Test
+    void multipleAssertsUsingJuniper() {
+        final Object o = Boolean.parseBoolean(System.getProperty("CREATE_NEW_OBJECT", "false")) ? new Object() : null;
+        assertAll(
+                () -> assertThat(o).describedAs("Is not null").isNotNull(),
+                () -> assertThat(o).describedAs("Is instance of").isInstanceOf(Object.class)
+        );
     }
 }
